5dc9bcb1489e8bcc9ccddc52b879cebbca669bc1
Hani Majebry <hanimajebry@MacBook-Pro-von-Hani.local>
2023-10-28 20:03:38 +0200

    eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/eloquent', function () {
+
+    // get a specific product
+    // return Product::find(2);
+
+    // accessing an attribute of the product
+    // $product = Product::find(3);
+    // return $product->name;
+
+    // or
+    // return Product::find(3)->name;
+
+    // get all products
+    // return Product::all();
+
+    // get latest 2 products
+    // return Product::latest('id')->limit(2)->get();
+
+    // get products ordered by specific column
+    // return Product::orderBy('price', 'ASC')->get()
+
+    // get latest product
+    // return Product::latest('id')->first();
+
+    // get products that has price over 20
+    // return Product::where('price', '>', 20)->get();
+
 });
